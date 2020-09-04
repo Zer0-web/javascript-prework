e071248fa313ee2313ed0f1778c9240de82ddcda
Mateusz Rozenkiewicz <rodzyn22@gmail.com>
2020-09-04 07:54:26 +0200

    add css, index.html, js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = `kamień`;
+let playerMove = `Papier! `;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage(playerMove + 'Wygrałem!');
